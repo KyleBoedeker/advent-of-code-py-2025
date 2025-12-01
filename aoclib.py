b75672bb59e09edbe0a31897407895a7ec0077f5
Kyle Boedeker <kyle.boedeker@silabs.com>
2025-12-01 15:02:56 -0600

    Add day1 part 2 solution

--- aoclib.py
+++ aoclib.py
@@ -13,3 +13,20 @@ def day01_part1(input: str):
             num_zeroes += 1
 
     print("Actual password:", num_zeroes)
+
+
+def day01_part2(input: str):
+    dial_position = 50
+    num_zero_crossings = 0
+
+    for line in input.splitlines():
+        dir = -1 if line[0] == "L" else 1
+        amt = int(line[1:])
+
+        for _ in range(amt):
+            dial_position += dir
+            dial_position %= 100
+            if dial_position == 0:
+                num_zero_crossings += 1
+
+    print("Actual password:", num_zero_crossings)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import aoclib
-from pathlib import Path
-import sys
 import argparse
+import sys
+from pathlib import Path
+
+import aoclib
 
 
 def main():
@@ -21,22 +22,26 @@ def main():
     )
     args = parser.parse_args()
 
+    if args.example:
+        puzzle_file = f"day{int(args.day):02d}_example.txt"
+    else:
+        puzzle_file = f"day{int(args.day):02d}_input.txt"
+
     try:
-        puzzle_file = (
-            f"{int(args.day):02d}_{int(args.part):d}"
-            + ("_e" if args.example else "")
-            + ".txt"
-        )
         puzzle = (Path(__file__).parent / "inputs" / puzzle_file).read_text()
     except FileNotFoundError:
         print(
-            f"Puzzle input not found for day {args.day} part {args.part}",
+            f"Puzzle input not found for Day {args.day} Part {args.part}",
+            f"Please create the file: input/{puzzle_file}",
+            sep="\n",
             file=sys.stderr,
         )
         exit(1)
 
     if args.day == "1" and args.part == "1":
         aoclib.day01_part1(puzzle)
+    elif args.day == "1" and args.part == "2":
+        aoclib.day01_part2(puzzle)
     else:
         print("unimplemented!", file=sys.stderr)
 
