77bc24d31b65a3ac4ea7b41c0a881c5641bf92ff
Kyle Boedeker <kyle.boedeker@silabs.com>
2025-12-01 15:14:10 -0600

    Refactor and add performance information

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 import argparse
 import sys
+import time
 from pathlib import Path
 
 import aoclib
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog="Advent of Code Solver",
-        description="Solves advent of code puzzles",
-    )
-
-    parser.add_argument(
-        "day", choices=list(map(str, range(1, 13))), help="Puzzle to solve: ex: '3'"
-    )
-    parser.add_argument(
-        "part", choices=["1", "2"], help="Puzzle part to solve (either 1 or 2)"
-    )
-    parser.add_argument(
-        "-e", "--example", action="store_true", help="Run the example instead"
-    )
-    args = parser.parse_args()
-
+def main(args: argparse.Namespace):
     if args.example:
         puzzle_file = f"day{int(args.day):02d}_example.txt"
     else:
@@ -38,6 +23,8 @@ def main():
         )
         exit(1)
 
+    t_start = time.time()
+
     if args.day == "1" and args.part == "1":
         aoclib.day01_part1(puzzle)
     elif args.day == "1" and args.part == "2":
@@ -45,6 +32,26 @@ def main():
     else:
         print("unimplemented!", file=sys.stderr)
 
+    if args.profile:
+        print(f"Solution ran in {time.time() - t_start:.3f} seconds.", file=sys.stderr)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        prog="Advent of Code Solver",
+        description="Solves advent of code puzzles",
+    )
+
+    parser.add_argument(
+        "day", choices=list(map(str, range(1, 13))), help="Puzzle to solve: ex: '3'"
+    )
+    parser.add_argument(
+        "part", choices=["1", "2"], help="Puzzle part to solve (either 1 or 2)"
+    )
+    parser.add_argument(
+        "-e", "--example", action="store_true", help="Run the example instead"
+    )
+    parser.add_argument(
+        "-p", "--profile", action="store_true", help="Print performance information"
+    )
+    args = parser.parse_args()
+    main(args)
