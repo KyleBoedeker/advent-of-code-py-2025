275abf386dc4a4708e1f9bf30156c8c1c93bf98e
Kyle Boedeker <kyle.boedeker@silabs.com>
2025-12-02 12:47:10 -0600

    Add day2 part 1 solution

--- aoclib.py
+++ aoclib.py
@@ -1,3 +1,6 @@
+import re
+
+
 def day01_part1(input: str):
     dial_position = 50
     num_zeroes = 0
@@ -30,3 +33,20 @@ def day01_part2(input: str):
                 num_zero_crossings += 1
 
     print("Actual password:", num_zero_crossings)
+
+
+def day02_is_invalid_id(num: str) -> bool:
+    if len(num) % 2 == 1:
+        return False
+    midpoint = len(num) // 2
+    return num[:midpoint] == num[midpoint:]
+
+
+def day02_part1(input: str):
+    sum_of_invalid_ids = 0
+    for match in re.finditer(r"(\d+)-(\d+)", input):
+        for num in range(int(match.group(1)), int(match.group(2)) + 1):
+            if day02_is_invalid_id(str(num)):
+                sum_of_invalid_ids += num
+
+    print("Sum of invalid IDs is:", sum_of_invalid_ids)
--- main.py
+++ main.py
@@ -29,11 +29,14 @@ def main(args: argparse.Namespace):
         aoclib.day01_part1(puzzle)
     elif args.day == "1" and args.part == "2":
         aoclib.day01_part2(puzzle)
+    elif args.day == "2" and args.part == "1":
+        aoclib.day02_part1(puzzle)
     else:
         print("unimplemented!", file=sys.stderr)
 
     if args.profile:
         print(f"Solution ran in {time.time() - t_start:.3f} seconds.", file=sys.stderr)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -41,17 +44,9 @@ if __name__ == "__main__":
         description="Solves advent of code puzzles",
     )
 
-    parser.add_argument(
-        "day", choices=list(map(str, range(1, 13))), help="Puzzle to solve: ex: '3'"
-    )
-    parser.add_argument(
-        "part", choices=["1", "2"], help="Puzzle part to solve (either 1 or 2)"
-    )
-    parser.add_argument(
-        "-e", "--example", action="store_true", help="Run the example instead"
-    )
-    parser.add_argument(
-        "-p", "--profile", action="store_true", help="Print performance information"
-    )
+    parser.add_argument("day", choices=list(map(str, range(1, 13))), help="Puzzle to solve: ex: '3'")
+    parser.add_argument("part", choices=["1", "2"], help="Puzzle part to solve (either 1 or 2)")
+    parser.add_argument("-e", "--example", action="store_true", help="Run the example instead")
+    parser.add_argument("-p", "--profile", action="store_true", help="Print performance information")
     args = parser.parse_args()
     main(args)
